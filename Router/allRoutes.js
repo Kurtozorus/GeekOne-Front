4b594e5b9b2880145ee41665559cf5b4f335f01d
Kurtozorus <olivier.marchand26@hotmail.fr>
2024-11-04 16:06:51 +0100

    Ajout systeme routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js";
+
+//Définir ici nos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html",)];
+
+//Le titre s'affiche comme ceci : Route.titre - Websitename
+export const websiteName = "GeekOne";
